18bbe4c84b1c8bb069dcc661de1ad1a16917927d
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-09-17 15:27:26 -0400

    Initial commit

---
+++ Krypton.hpp
@@ -0,0 +1,16 @@
+
+#ifndef PROJECT_INCLUDES_KRYPTON_HPP
+#define PROJECT_INCLUDES_KRYPTON_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/program_options.hpp>
+
+#endif // PROJECT_INCLUDES_KRYPTON_HPP
+
